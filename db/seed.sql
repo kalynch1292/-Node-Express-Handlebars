da544f43d2c2be74e7339fe40251be03548debb5
Kayla Lynch <kalynch1292@gmail.com>
2020-12-12 12:00:47 -0500

    updates from studying class work

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured, date) 
+VALUES ('Cheeseburger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) 
+VALUES ('Veggie Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) 
+VALUES ('Impossible Burger', false, CURRENT_TIMESTAMP);
